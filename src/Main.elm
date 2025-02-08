c36b9d9eaaeb606347f7707935b6950012ab57bc
Thomas Volk (MacBook) <info@thomasvolk.de>
2025-02-08 20:08:25 +0100

    model

--- src/Main.elm
+++ src/Main.elm
@@ -5,22 +5,29 @@ import Html exposing (Html, input, button, div, text)
 import Html.Events exposing (onClick)
 
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
+  Browser.sandbox { init = init, update = update, view = view }
+
+type alias Model =
+  { score : Int }
+    
+init : Model
+init = 
+  { score = 1 }
 
 type Msg = Increment | Decrement
 
 update msg model =
   case msg of
     Increment ->
-      model + 1
+      { score = model.score + 1 }
 
     Decrement ->
-      model - 1
+      { score = model.score - 1 }
 
 view model =
   div []
     [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
+    , div [] [ text (String.fromInt model.score) ]
     , button [ onClick Increment ] [ text "+" ]
     ]
 
