c77d3c301ef6212e8c7e2b82e91099a858440fae
Thomas Volk (MacBook) <info@thomasvolk.de>
2025-02-08 21:31:05 +0100

    format

--- src/Main.elm
+++ src/Main.elm
@@ -1,44 +1,59 @@
 port module Main exposing (main)
 
 import Browser
-import Html exposing (Html, input, button, div, text)
+import Html exposing (Html, button, div, input, text)
 import Html.Events exposing (onClick)
 
+
 main =
-  Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
+    Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
+
 
 type alias Model =
-  { score : Int }
-    
-init : () -> ( Model, Cmd Msg )
-init _ = 
-  ({ score = 1 }, Cmd.none)
+    { score : Int }
 
-type Msg = Increment | SendScore | ReceiveScore Int
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { score = 1 }, Cmd.none )
+
+
+type Msg
+    = Increment
+    | SendScore
+    | ReceiveScore Int
+
 
 port scoreOut : Int -> Cmd msg
 
+
 port scoreIn : (Int -> msg) -> Sub msg
+
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-    scoreIn ReceiveScore 
+    scoreIn ReceiveScore
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Increment ->
-      let newScore = model.score + 1 in
-      ({ score = newScore }, scoreOut newScore)
-    SendScore ->
-      ({ score = model.score }, scoreOut model.score)
-    ReceiveScore score ->
-      ({ score = score }, Cmd.none)
+    case msg of
+        Increment ->
+            let
+                newScore =
+                    model.score + 1
+            in
+            ( { score = newScore }, scoreOut newScore )
+
+        SendScore ->
+            ( { score = model.score }, scoreOut model.score )
+
+        ReceiveScore score ->
+            ( { score = score }, Cmd.none )
 
 
 view model =
-  div []
-    [ div [] [ text (String.fromInt model.score) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
-
+    div []
+        [ div [] [ text (String.fromInt model.score) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
